eb2b8e5754a56522df16fc114b0872c682f5f42c
Dydo Vitalii <vitalik@MacBook-Pro-Dydo.local>
2022-04-28 09:21:03 +0300

    The project is complited

---
+++ app/src/main/java/com/example/foodapp/SuccessOrderActivity.kt
@@ -0,0 +1,25 @@
+package com.example.foodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.example.foodapp.models.RestaurantModel
+import kotlinx.android.synthetic.main.activity_success_order.*
+
+class SuccessOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success_order)
+
+        val restaurantModel: RestaurantModel? = intent.getParcelableExtra("RestaurantModel")
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setTitle(restaurantModel?.name)
+        actionBar?.setSubtitle(restaurantModel?.address)
+        actionBar?.setDisplayHomeAsUpEnabled(false)
+
+        buttonDone.setOnClickListener {
+         setResult(RESULT_OK)
+         finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/foodapp/adapter/PlaceYourOrderAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.foodapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.foodapp.R
+import com.example.foodapp.models.Menus
+
+class PlaceYourOrderAdapter(val menuList: List<Menus?>?):RecyclerView.Adapter<PlaceYourOrderAdapter.MyViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): PlaceYourOrderAdapter.MyViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.placeyourorder_list_row, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PlaceYourOrderAdapter.MyViewHolder, position: Int) {
+       holder.bind(menuList?.get(position)!!)
+    }
+
+    override fun getItemCount(): Int {
+         return if (menuList == null) 0 else menuList.size
+    }
+
+    inner class MyViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val thumbImage: ImageView = view.findViewById(R.id.thumbImage)
+        val menuName: TextView = view.findViewById(R.id.menuName)
+        val menuPrice: TextView = view.findViewById(R.id.menuPrice)
+        val menuQty: TextView = view.findViewById(R.id.menuQty)
+
+        fun bind(menu:Menus){
+           Glide.with(thumbImage).load(menu?.url).into(thumbImage)
+           menuName.text = menu?.name!!
+           menuPrice.text = "Price $" + String.format("%.2f",menu?.price * menu?.totalInCart)
+           menuQty.text = "Qty :" + menu?.totalInCart
+        }
+    }
+
+}
